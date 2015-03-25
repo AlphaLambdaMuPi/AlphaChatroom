3f26751017eff05929dd7718707a2df857e7c5bd
Meteor <bobogei81123@gmail.com>
2015-03-25 11:42:05 +0800

    add client logger

--- client/main.py
+++ client/main.py
@@ -3,34 +3,51 @@
 import sys
 import math
 import time
+import asyncio
+import json
+
+import logging
+LOG_FILE_NAME = 'client.log'
+logging.basicConfig(filename=LOG_FILE_NAME,
+                    filemode='a',
+                    format='[%(asctime)s.%(msecs)d] %(name)s - %(levelname)s : %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtQml import *
 from PyQt5.QtQuick import *
-import asyncio
+
 from quamash import QEventLoop
-import json
 
 @asyncio.coroutine
-def zzz():
+def test_el():
     i = 0
     while True:
         print(i)
         yield from asyncio.sleep(0.5)
         i += 1
 
+        if i > 10:
+            return
+
 @asyncio.coroutine
 def tcp_client_connect():
-    r, w = yield from asyncio.open_connection('140.112.18.210', 9007)
-    x = { 'abc': 'alphabetagamma',
-            'nick': 'zzzzzzz'}
-    while True:
-        print(123)
-        mesg = json.dumps(x) + '\n'
-        w.write(mesg.encode())
 
-        data = yield from r.readline()
-        print('456', data.decode())
+    try:
+        reader, writer = yield from asyncio.open_connection('140.112.18.210', 9007)
+    except Exception as e:
+        logger.error(e)
+        return
+
+    while True:
+        mesg = json.dumps(x) + '\n'
+        writer.write(mesg.encode())
+        logger.debug(1, 2)
+
+        data = yield from reader.readline()
 
 class Logic(QObject):
     def __init__(self):
@@ -39,56 +56,52 @@ class Logic(QObject):
 
     @pyqtSlot()
     def hello(self):
-        print('Hello!')
         loop = asyncio.get_event_loop()
 
-
-        print('F1: ', loop.is_running())
+        logger.debug('Start Connect')
         try:
             loop.create_task(tcp_client_connect())
         except Exception as e:
-            print(e)
-            print('F2: ', loop.is_running())
+            logger.error(e)
 
 
+def init_logging():
+    formatter = logging.Formatter(fmt = '[%(asctime)s.%(msecs)d] %(name)s - %(levelname)s : %(message)s'
+                                  ,datefmt = '%H:%M:%S')
+    console = logging.StreamHandler(sys.stdout)
+    console.setLevel(logging.DEBUG)
+    console.setFormatter(formatter)
+    logger.addHandler(console)
+    logger.debug('Logging Config Done')
 
-def main():
+def app_setup():
+    '''
+    Init app window setups goes here...
+    '''
+
     app = QApplication(sys.argv)
+
     loop = QEventLoop(app)
     asyncio.set_event_loop(loop)
+
     engine = QQmlApplicationEngine()
     engine.load(QUrl('main_gui.qml'))
     topLevel = engine.rootObjects()[0]
 
     logic = Logic()
     engine.rootContext().setContextProperty('logic', logic)
+
     topLevel.show()
 
-    x = { 'abc': 'alphabetagamma',
-         'nick': 'zzzzzzz'}
+    app.aboutToQuit.connect(lambda: loop.close())
 
-    @asyncio.coroutine
-    def tcp_client_connect():
-        r, w = yield from asyncio.open_connection('140.112.18.210', 9007)
-        while True:
-            mesg = json.dumps(x) + '\n'
-            print(mesg)
-            w.write(mesg.encode())
+    app.exec_()
 
-            data = yield from r.readline()
-            print(data.decode())
+def main():
+
+    init_logging()
+
+    app_setup()
 
 
-
-    #appView = QQuickView()
-    #appView.setSource(QUrl('main_gui.qml'))
-    #appView.show()
-    #main = MainHandle()
-    #appView.rootContext().setContextProperty('main', main)
-
-
-
-    loop.run_until_complete(app.exec_())
-
-if __name__ == '__main__':
-    main()
+main()
