a0acf156a0e39b8f36b7ed654ad45fe3646156c3
step5 <kevin00050@gmail.com>
2015-03-26 16:12:09 +0800

    pyqtdply

--- client/build/AlphaChatroom.pro
+++ client/build/AlphaChatroom.pro
@@ -20,8 +20,9 @@ win32 {
     QMAKE_EXTRA_COMPILERS += masm
 }
 
-CONFIG += warn_off console release
-QT -= gui
+CONFIG += warn_off release
+CONFIG -= app_bundle
+QT += quick quickwidgets
 
 RESOURCES = \
     resources/pyqtdeploy.qrc
@@ -32,73 +33,78 @@ DEFINES += PYQTDEPLOY_OPTIMIZED
 HEADERS = pyqtdeploy_version.h frozen_bootstrap.h frozen_main.h
 
 DEFINES += XML_STATIC
-INCLUDEPATH += /src/Python-3.4.3/Modules/_multiprocessing
-INCLUDEPATH += /src/Python-3.4.3/Modules
-INCLUDEPATH += /src/Python-3.4.3/Modules/_ctypes
-INCLUDEPATH += /usr/include/python3.4m
 INCLUDEPATH += /src/Python-3.4.3/Modules/expat
+INCLUDEPATH += /src/Python-3.4.3/Modules/_ctypes
+INCLUDEPATH += /src/Python-3.4.3/Modules/_multiprocessing
+INCLUDEPATH += /home/step5/python_static/include/python3.4m
+INCLUDEPATH += /src/Python-3.4.3/Modules
 LIBS += -lssl
 LIBS += -lcrypto
-SOURCES += /src/Python-3.4.3/Modules/selectmodule.c
-SOURCES += /src/Python-3.4.3/Modules/pyexpat.c
-SOURCES += /src/Python-3.4.3/Modules/_ctypes/callbacks.c
+LIBS += -L/home/step5/python_static/lib/python3.4/site-packages -lsip
+LIBS += -lffi
+LIBS += -L/home/step5/python_static/lib/python3.4/site-packages/PyQt5 -lQt -lQtGui -lQtCore -lQtQml -lQtNetwork -lQtQuick -lQtWidgets -lQtQuickWidgets
+LIBS += -lreadline
 SOURCES += /src/Python-3.4.3/Modules/sha256module.c
-SOURCES += /src/Python-3.4.3/Modules/sha512module.c
-SOURCES += /src/Python-3.4.3/Modules/_ctypes/cfield.c
+SOURCES += /src/Python-3.4.3/Modules/pyexpat.c
 SOURCES += /src/Python-3.4.3/Modules/mathmodule.c
-SOURCES += /src/Python-3.4.3/Modules/timemodule.c
-SOURCES += /src/Python-3.4.3/Modules/_multiprocessing/multiprocessing.c
-SOURCES += /src/Python-3.4.3/Modules/_ctypes/callproc.c
-SOURCES += /src/Python-3.4.3/Modules/_multiprocessing/semaphore.c
 SOURCES += /src/Python-3.4.3/Modules/_math.c
-SOURCES += /src/Python-3.4.3/Modules/_json.c
-SOURCES += /src/Python-3.4.3/Modules/arraymodule.c
-SOURCES += /src/Python-3.4.3/Modules/_randommodule.c
-SOURCES += /src/Python-3.4.3/Modules/binascii.c
-SOURCES += /src/Python-3.4.3/Modules/_pickle.c
-SOURCES += /src/Python-3.4.3/Modules/_ssl.c
-SOURCES += /src/Python-3.4.3/Modules/_datetimemodule.c
-SOURCES += /src/Python-3.4.3/Modules/_ctypes/stgdict.c
+SOURCES += /src/Python-3.4.3/Modules/_ctypes/cfield.c
 SOURCES += /src/Python-3.4.3/Modules/expat/xmltok.c
+SOURCES += /src/Python-3.4.3/Modules/timemodule.c
+SOURCES += /src/Python-3.4.3/Modules/_ctypes/callbacks.c
+SOURCES += /src/Python-3.4.3/Modules/_ctypes/stgdict.c
+SOURCES += /src/Python-3.4.3/Modules/_ctypes/callproc.c
+SOURCES += /src/Python-3.4.3/Modules/_ssl.c
+SOURCES += /src/Python-3.4.3/Modules/_multiprocessing/multiprocessing.c
+SOURCES += /src/Python-3.4.3/Modules/arraymodule.c
 SOURCES += /src/Python-3.4.3/Modules/_heapqmodule.c
+SOURCES += /src/Python-3.4.3/Modules/mmapmodule.c
 SOURCES += /src/Python-3.4.3/Modules/expat/xmlparse.c
 SOURCES += /src/Python-3.4.3/Modules/_bisectmodule.c
-SOURCES += /src/Python-3.4.3/Modules/expat/xmlrole.c
-SOURCES += /src/Python-3.4.3/Modules/_ctypes/_ctypes.c
-SOURCES += /src/Python-3.4.3/Modules/mmapmodule.c
 SOURCES += /src/Python-3.4.3/Modules/sha1module.c
-SOURCES += /src/Python-3.4.3/Modules/socketmodule.c
+SOURCES += /src/Python-3.4.3/Modules/selectmodule.c
+SOURCES += /src/Python-3.4.3/Modules/_json.c
+SOURCES += /src/Python-3.4.3/Modules/expat/xmlrole.c
+SOURCES += /src/Python-3.4.3/Modules/_multiprocessing/semaphore.c
+SOURCES += /src/Python-3.4.3/Modules/_randommodule.c
+SOURCES += /src/Python-3.4.3/Modules/binascii.c
+SOURCES += /src/Python-3.4.3/Modules/_datetimemodule.c
+SOURCES += /src/Python-3.4.3/Modules/sha512module.c
 SOURCES += /src/Python-3.4.3/Modules/md5module.c
+SOURCES += /src/Python-3.4.3/Modules/_pickle.c
+SOURCES += /src/Python-3.4.3/Modules/_ctypes/_ctypes.c
+SOURCES += /src/Python-3.4.3/Modules/socketmodule.c
 
 !win32 {
     DEFINES += HAVE_EXPAT_CONFIG_H
-    LIBS += -L/usr/lib -lpython3.4m
+    LIBS += -L/home/step5/python_static/lib -lpython3.4m
     SOURCES += /src/Python-3.4.3/Modules/_posixsubprocess.c
+    SOURCES += /src/Python-3.4.3/Modules/readline.c
     SOURCES += /src/Python-3.4.3/Modules/grpmodule.c
+}
+
+win32 {
+    DEFINES += COMPILED_FROM_DSP
+    INCLUDEPATH += /src/Python-3.4.3/Modules/_ctypes/libffi_msvc
+    LIBS += -L/home/step5/python_static/lib -lpython34m
+    SOURCES += /src/Python-3.4.3/Modules/_ctypes/malloc_closure.c
+    SOURCES += /src/Python-3.4.3/Modules/_ctypes/libffi_msvc/prep_cif.c
+    SOURCES += /src/Python-3.4.3/Modules/_ctypes/libffi_msvc/ffi.c
+    SOURCES += /src/Python-3.4.3/PC/msvcrtmodule.c
+    SOURCES += /src/Python-3.4.3/Modules/_winapi.c
 }
 
 macx {
     DEFINES += MACOSX
     INCLUDEPATH += /src/Python-3.4.3/Modules/_ctypes/darwin
     INCLUDEPATH += /src/Python-3.4.3/Modules/_ctypes/libffi_osx/include
+    SOURCES += /src/Python-3.4.3/Modules/_ctypes/malloc_closure.c
     SOURCES += /src/Python-3.4.3/Modules/_ctypes/libffi_osx/x86/x86-darwin.S
-    SOURCES += /src/Python-3.4.3/Modules/_ctypes/libffi_osx/x86/x86-ffi_darwin.c
-    SOURCES += /src/Python-3.4.3/Modules/_ctypes/libffi_osx/ffi.c
     SOURCES += /src/Python-3.4.3/Modules/_ctypes/libffi_osx/x86/x86-ffi64.c
-    SOURCES += /src/Python-3.4.3/Modules/_ctypes/libffi_osx/x86/darwin64.S
-    SOURCES += /src/Python-3.4.3/Modules/_ctypes/malloc_closure.c
+    SOURCES += /src/Python-3.4.3/Modules/_ctypes/libffi_osx/x86/x86-ffi_darwin.c
     SOURCES += /src/Python-3.4.3/Modules/_ctypes/darwin/dlfcn_simple.c
-}
-
-win32 {
-    DEFINES += COMPILED_FROM_DSP
-    INCLUDEPATH += /src/Python-3.4.3/Modules/_ctypes/libffi_msvc
-    LIBS += -L/usr/lib -lpython34m
-    SOURCES += /src/Python-3.4.3/Modules/_ctypes/libffi_msvc/prep_cif.c
-    SOURCES += /src/Python-3.4.3/PC/msvcrtmodule.c
-    SOURCES += /src/Python-3.4.3/Modules/_ctypes/libffi_msvc/ffi.c
-    SOURCES += /src/Python-3.4.3/Modules/_winapi.c
-    SOURCES += /src/Python-3.4.3/Modules/_ctypes/malloc_closure.c
+    SOURCES += /src/Python-3.4.3/Modules/_ctypes/libffi_osx/ffi.c
+    SOURCES += /src/Python-3.4.3/Modules/_ctypes/libffi_osx/x86/darwin64.S
 }
 
 linux-* {
--- client/build/pyqtdeploy_main.cpp
+++ client/build/pyqtdeploy_main.cpp
@@ -2,128 +2,168 @@
 #include <QtGlobal>
 
 #if PY_MAJOR_VERSION >= 3
-extern "C" PyObject *PyInit__json(void);
-extern "C" PyObject *PyInit_binascii(void);
-extern "C" PyObject *PyInit__datetime(void);
-extern "C" PyObject *PyInit__sha1(void);
-extern "C" PyObject *PyInit__pickle(void);
 extern "C" PyObject *PyInit_pyexpat(void);
-extern "C" PyObject *PyInit__md5(void);
-extern "C" PyObject *PyInit__multiprocessing(void);
-extern "C" PyObject *PyInit__ctypes(void);
+extern "C" PyObject *PyInit__socket(void);
 extern "C" PyObject *PyInit__ssl(void);
+extern "C" PyObject *PyInit__multiprocessing(void);
+extern "C" PyObject *PyInit_QtQuickWidgets(void);
+extern "C" PyObject *PyInit_QtWidgets(void);
+extern "C" PyObject *PyInit_math(void);
+extern "C" PyObject *PyInit__bisect(void);
+extern "C" PyObject *PyInit_QtCore(void);
+extern "C" PyObject *PyInit__pickle(void);
+extern "C" PyObject *PyInit_binascii(void);
+extern "C" PyObject *PyInit__json(void);
+extern "C" PyObject *PyInit_array(void);
+extern "C" PyObject *PyInit_Qt(void);
+extern "C" PyObject *PyInit__sha512(void);
 extern "C" PyObject *PyInit_mmap(void);
 extern "C" PyObject *PyInit_select(void);
-extern "C" PyObject *PyInit__sha256(void);
 extern "C" PyObject *PyInit__random(void);
-extern "C" PyObject *PyInit_array(void);
-extern "C" PyObject *PyInit__socket(void);
-extern "C" PyObject *PyInit_math(void);
-extern "C" PyObject *PyInit__heapq(void);
-extern "C" PyObject *PyInit__sha512(void);
+extern "C" PyObject *PyInit__sha256(void);
+extern "C" PyObject *PyInit__sha1(void);
+extern "C" PyObject *PyInit_QtGui(void);
+extern "C" PyObject *PyInit_QtNetwork(void);
+extern "C" PyObject *PyInit__datetime(void);
+extern "C" PyObject *PyInit_sip(void);
+extern "C" PyObject *PyInit__md5(void);
+extern "C" PyObject *PyInit__ctypes(void);
+extern "C" PyObject *PyInit_QtQml(void);
+extern "C" PyObject *PyInit_QtQuick(void);
 extern "C" PyObject *PyInit_time(void);
-extern "C" PyObject *PyInit__bisect(void);
+extern "C" PyObject *PyInit__heapq(void);
 #if !defined(Q_OS_WIN)
-extern "C" PyObject *PyInit__posixsubprocess(void);
+extern "C" PyObject *PyInit_readline(void);
 extern "C" PyObject *PyInit_grp(void);
+extern "C" PyObject *PyInit__posixsubprocess(void);
 #endif
 #if defined(Q_OS_WIN)
-extern "C" PyObject *PyInit_msvcrt(void);
 extern "C" PyObject *PyInit__winapi(void);
+extern "C" PyObject *PyInit_msvcrt(void);
 #endif
 
 static struct _inittab extension_modules[] = {
-    {"_json", PyInit__json},
-    {"binascii", PyInit_binascii},
-    {"_datetime", PyInit__datetime},
-    {"_sha1", PyInit__sha1},
-    {"_pickle", PyInit__pickle},
     {"pyexpat", PyInit_pyexpat},
-    {"_md5", PyInit__md5},
-    {"_multiprocessing", PyInit__multiprocessing},
-    {"_ctypes", PyInit__ctypes},
+    {"_socket", PyInit__socket},
     {"_ssl", PyInit__ssl},
+    {"_multiprocessing", PyInit__multiprocessing},
+    {"PyQt5.QtQuickWidgets", PyInit_QtQuickWidgets},
+    {"PyQt5.QtWidgets", PyInit_QtWidgets},
+    {"math", PyInit_math},
+    {"_bisect", PyInit__bisect},
+    {"PyQt5.QtCore", PyInit_QtCore},
+    {"_pickle", PyInit__pickle},
+    {"binascii", PyInit_binascii},
+    {"_json", PyInit__json},
+    {"array", PyInit_array},
+    {"PyQt5.Qt", PyInit_Qt},
+    {"_sha512", PyInit__sha512},
     {"mmap", PyInit_mmap},
     {"select", PyInit_select},
-    {"_sha256", PyInit__sha256},
     {"_random", PyInit__random},
-    {"array", PyInit_array},
-    {"_socket", PyInit__socket},
-    {"math", PyInit_math},
-    {"_heapq", PyInit__heapq},
-    {"_sha512", PyInit__sha512},
+    {"_sha256", PyInit__sha256},
+    {"_sha1", PyInit__sha1},
+    {"PyQt5.QtGui", PyInit_QtGui},
+    {"PyQt5.QtNetwork", PyInit_QtNetwork},
+    {"_datetime", PyInit__datetime},
+    {"sip", PyInit_sip},
+    {"_md5", PyInit__md5},
+    {"_ctypes", PyInit__ctypes},
+    {"PyQt5.QtQml", PyInit_QtQml},
+    {"PyQt5.QtQuick", PyInit_QtQuick},
     {"time", PyInit_time},
-    {"_bisect", PyInit__bisect},
+    {"_heapq", PyInit__heapq},
 #if !defined(Q_OS_WIN)
-    {"_posixsubprocess", PyInit__posixsubprocess},
+    {"readline", PyInit_readline},
     {"grp", PyInit_grp},
+    {"_posixsubprocess", PyInit__posixsubprocess},
 #endif
 #if defined(Q_OS_WIN)
-    {"msvcrt", PyInit_msvcrt},
     {"_winapi", PyInit__winapi},
+    {"msvcrt", PyInit_msvcrt},
 #endif
     {NULL, NULL}
 };
 #else
-extern "C" void init_json(void);
-extern "C" void initbinascii(void);
-extern "C" void init_datetime(void);
-extern "C" void init_sha1(void);
-extern "C" void init_pickle(void);
 extern "C" void initpyexpat(void);
-extern "C" void init_md5(void);
-extern "C" void init_multiprocessing(void);
-extern "C" void init_ctypes(void);
+extern "C" void init_socket(void);
 extern "C" void init_ssl(void);
+extern "C" void init_multiprocessing(void);
+extern "C" void initQtQuickWidgets(void);
+extern "C" void initQtWidgets(void);
+extern "C" void initmath(void);
+extern "C" void init_bisect(void);
+extern "C" void initQtCore(void);
+extern "C" void init_pickle(void);
+extern "C" void initbinascii(void);
+extern "C" void init_json(void);
+extern "C" void initarray(void);
+extern "C" void initQt(void);
+extern "C" void init_sha512(void);
 extern "C" void initmmap(void);
 extern "C" void initselect(void);
-extern "C" void init_sha256(void);
 extern "C" void init_random(void);
-extern "C" void initarray(void);
-extern "C" void init_socket(void);
-extern "C" void initmath(void);
-extern "C" void init_heapq(void);
-extern "C" void init_sha512(void);
+extern "C" void init_sha256(void);
+extern "C" void init_sha1(void);
+extern "C" void initQtGui(void);
+extern "C" void initQtNetwork(void);
+extern "C" void init_datetime(void);
+extern "C" void initsip(void);
+extern "C" void init_md5(void);
+extern "C" void init_ctypes(void);
+extern "C" void initQtQml(void);
+extern "C" void initQtQuick(void);
 extern "C" void inittime(void);
-extern "C" void init_bisect(void);
+extern "C" void init_heapq(void);
 #if !defined(Q_OS_WIN)
-extern "C" void init_posixsubprocess(void);
+extern "C" void initreadline(void);
 extern "C" void initgrp(void);
+extern "C" void init_posixsubprocess(void);
 #endif
 #if defined(Q_OS_WIN)
-extern "C" void initmsvcrt(void);
 extern "C" void init_winapi(void);
+extern "C" void initmsvcrt(void);
 #endif
 
 static struct _inittab extension_modules[] = {
-    {"_json", init_json},
-    {"binascii", initbinascii},
-    {"_datetime", init_datetime},
-    {"_sha1", init_sha1},
-    {"_pickle", init_pickle},
     {"pyexpat", initpyexpat},
-    {"_md5", init_md5},
-    {"_multiprocessing", init_multiprocessing},
-    {"_ctypes", init_ctypes},
+    {"_socket", init_socket},
     {"_ssl", init_ssl},
+    {"_multiprocessing", init_multiprocessing},
+    {"PyQt5.QtQuickWidgets", initQtQuickWidgets},
+    {"PyQt5.QtWidgets", initQtWidgets},
+    {"math", initmath},
+    {"_bisect", init_bisect},
+    {"PyQt5.QtCore", initQtCore},
+    {"_pickle", init_pickle},
+    {"binascii", initbinascii},
+    {"_json", init_json},
+    {"array", initarray},
+    {"PyQt5.Qt", initQt},
+    {"_sha512", init_sha512},
     {"mmap", initmmap},
     {"select", initselect},
-    {"_sha256", init_sha256},
     {"_random", init_random},
-    {"array", initarray},
-    {"_socket", init_socket},
-    {"math", initmath},
-    {"_heapq", init_heapq},
-    {"_sha512", init_sha512},
+    {"_sha256", init_sha256},
+    {"_sha1", init_sha1},
+    {"PyQt5.QtGui", initQtGui},
+    {"PyQt5.QtNetwork", initQtNetwork},
+    {"_datetime", init_datetime},
+    {"sip", initsip},
+    {"_md5", init_md5},
+    {"_ctypes", init_ctypes},
+    {"PyQt5.QtQml", initQtQml},
+    {"PyQt5.QtQuick", initQtQuick},
     {"time", inittime},
-    {"_bisect", init_bisect},
+    {"_heapq", init_heapq},
 #if !defined(Q_OS_WIN)
-    {"_posixsubprocess", init_posixsubprocess},
+    {"readline", initreadline},
     {"grp", initgrp},
+    {"_posixsubprocess", init_posixsubprocess},
 #endif
 #if defined(Q_OS_WIN)
-    {"msvcrt", initmsvcrt},
     {"_winapi", init_winapi},
+    {"msvcrt", initmsvcrt},
 #endif
     {NULL, NULL}
 };
